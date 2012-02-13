43fc7e795afe7f677de8f6260ef4645bad86145a
Rich Leland <rich@richleland.com>
2012-02-12 22:56:34 -0500

    Added basics to begin multi-mech testing

---
+++ multimechanize/test/test_multimech.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+def test_something():
+    assert (1 == 3)
+
+
+class TestMultiMech(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
